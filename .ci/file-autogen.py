fa1647f37ab8d5f29c7ec31d8a382b99b95aaaa5
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-01-07 19:49:36 -0600

    Migrate to XML

--- .ci/file-autogen.py
+++ .ci/file-autogen.py
@@ -1,17 +1,20 @@
 import glob
 from pathlib import Path
-import yaml
 import os
-import json
+import xml.etree.ElementTree as ET
+import sys
 
 if __name__ == '__main__':
-  for patches in glob.glob('patches/json/*.json', recursive=True):
-    app_titleid_str = 'app_titleid'
-    with open(patches, 'r') as file:
-      patch_file = json.load(file)
-      json_id = patch_file["patch"]
-      patch_file['base_file'] = patches
-      for i in range(0, len(json_id[0][app_titleid_str])):
-        out = ('output/json/{}.json'.format(json_id[0][app_titleid_str][i]))
-        with open(out, 'w') as fw:
-          fw.write(json.dumps(patch_file, indent=2))
+    if len(sys.argv) == 2:
+        for patches in glob.glob(sys.argv[1], recursive=True):
+            with open(patches, 'r') as fr:
+                string_data = fr.read()
+                root = ET.fromstring(string_data)
+                ID = root.findall('TitleID/ID')
+                for i in ID:
+                    out = ('output/xml/{}.xml'.format(i.text))
+                    print(out)
+                    with open(out, 'w') as fw:
+                        fw.write(f'{string_data}')
+    else:
+        print('path to glob not provided')
